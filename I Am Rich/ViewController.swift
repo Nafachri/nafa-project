4a2bfd30847c9f72832ced4f863fdc0028f76173
Nafachri <nafa.create@gmail.com>
2024-05-21 02:12:17 +0700

    initial commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Naufal Al-Fachri on 18/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
